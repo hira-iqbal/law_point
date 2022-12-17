88ca5778fa0bbf7b0f0f20f7b1e495828da1ccc7
hira <hirach502@gmail.com>
2022-12-18 00:55:27 +0500

    index page enhancments

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
 
   validates :user_id, presence: true
   validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :body, presence: true, length: { minimum: 200 }
 
   scope :ordered, -> { order(updated_at: :desc) }
 end
