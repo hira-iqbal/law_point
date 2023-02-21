246b9e6b7d609321e79f6dc46d630b3120243a45
hira <hirach502@gmail.com>
2023-02-21 20:14:26 +0500

    update login page and other fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
   protected
 
   def update_allowed_parameters
